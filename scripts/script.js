7956420c23ec1ed3df5cf232c06101c0609d81e3
Yuliia Shylova <yuliashilova22@gmail.com>
2018-03-27 21:32:24 +0300

    accordeon v2.0

--- scripts/script.js
+++ scripts/script.js
@@ -1,19 +1,31 @@
-var previousElement = null;
+let previousElement = null;
 [...document.getElementsByClassName('species')]
 .forEach((element) => element.onclick = (() => {
-	
-	if (previousElement) {		
-		previousElement.className = 'species';
-		previousElement.nextElementSibling.style.height = '0';		
-	}
-	element.className = 'species species-current';
+	hideContent(previousElement);
+	showContent(element);
+	previousElement = element;
+}));
 
+function changeBorder(element) {
+	let listener = () => { 
+		element.className = 'species';
+		element.nextElementSibling.removeEventListener('transitionend', listener);
+	};
+	element.nextElementSibling.addEventListener('transitionend', listener);
+}
+
+function hideContent(element) {
+	if (element) {		
+		changeBorder(element);
+		element.nextElementSibling.style.height = '0';
+	}
+}
+
+function showContent(element) {
 	let wrapper = element.nextElementSibling;
 	let aboutCat = wrapper.firstElementChild;
 
+	element.className = 'species species-current';
 	aboutCat.className = 'about-cat about-cat-current';
 	wrapper.style.height = aboutCat.offsetHeight  + 'px';
-	previousElement = element;
-
-}));
-
+}
